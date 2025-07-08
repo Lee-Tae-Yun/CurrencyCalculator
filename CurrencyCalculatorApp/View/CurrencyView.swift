4a3ecab3a14b85c42608df0670b00275b0eaa91c
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-07-08 21:16:06 +0900

    ✨ Feat: Lv1 메인 UI 기초 작업 + 데이터 불러오기
    UITableView, UILabel로 메인 UI 기초 작업을 하고, Alamofire로 데이터를 받아왔습니다.
    
    Closes #2

--- CurrencyCalculatorApp/View/CurrencyView.swift
+++ CurrencyCalculatorApp/View/CurrencyView.swift
@@ -4,4 +4,37 @@
 //
 //  Created by 이태윤 on 7/7/25.
 //
+import UIKit
+import Then
+
+class CurrencyView: UIView {
+
+  lazy var currencyTableView = UITableView().then {
+    $0.register(CurrencyTableViewCell.self, forCellReuseIdentifier: CurrencyTableViewCell.id)
+  }
+
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    backgroundColor = .systemBackground
+    addViewUI()
+    tableViewConfigure()
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+  private func addViewUI() {
+    addSubview(currencyTableView)
+  }
+
+  // MARK: - TavbleView
+  private func tableViewConfigure() {
+    currencyTableView.snp.makeConstraints {
+      $0.directionalEdges.equalTo(safeAreaLayoutGuide)
+    }
+  }
+}
+
 
--- CurrencyCalculatorApp/View/CurrencyViewController.swift
+++ CurrencyCalculatorApp/View/CurrencyViewController.swift
@@ -7,12 +7,65 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+  private let currencyService = CurrencyService()
+  private let currencyView = CurrencyView()
+  private var items: [CurrencyItem] = []
+  private var base = CurrencyBase.krw
+
+  override func loadView() {
+    view = currencyView
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    print("Hello world")
+    currencyView.currencyTableView.delegate = self
+    currencyView.currencyTableView.dataSource = self
+    loadData(for: base)
   }
-}
 
+  func loadData(for base: CurrencyBase) {
+    currencyService.fetchCurrency(base: base) { result in
+      switch result {
+      case .success(let currency):
+        DispatchQueue.main.async {
+          self.items = currency.rates.map { CurrencyItem(code: $0.key, rate: $0.value) }
+          self.currencyView.currencyTableView.reloadData()
+        }
+      case .failure(let error):
+        print("❌ 에러 발생: \(error)")
+          DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error", message: "데이터를 불러오지 못했습니다.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .default))
+            self.present(alert, animated: true)
+          }
+        }
+      }
+    }
+  }
+
+  extension ViewController: UITableViewDelegate {
+    // 셀을 선택했을 때
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+      print("선택한 항목: \(items[indexPath.row])")
+    }
+    // 테이블 뷰 셀의 높이 크기 지정.
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+      30
+    }
+  }
+
+  extension ViewController: UITableViewDataSource {
+    // 각셀에 어떤 데이터를 넣을껀지.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       guard let cell = tableView.dequeueReusableCell(withIdentifier: CurrencyTableViewCell.id, for: indexPath) as? CurrencyTableViewCell else { return UITableViewCell() }
+       let item = items[indexPath.row]
+       cell.configureCell(code: item.code, rate: item.rate)
+       return cell
+     }
+    // 총 몇줄인지
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return items.count
+    }
+  }
