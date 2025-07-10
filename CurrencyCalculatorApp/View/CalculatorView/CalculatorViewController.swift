837e8a05964a33999968c09ee546410ea7387f7a
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-07-10 20:19:00 +0900

    ✨ Feat: 버튼 클릭시 환율계산 Closes #10

--- CurrencyCalculatorApp/View/CalculatorView/CalculatorViewController.swift
+++ CurrencyCalculatorApp/View/CalculatorView/CalculatorViewController.swift
@@ -19,5 +19,24 @@ class CalculatorViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     calculator.calcuatorConfigure(code: countryCode ?? "국가코드 없음", country: countryName ?? "국가명 없음")
+    calculator.convertButton.addTarget(self, action: #selector(convertButtonDidTap), for: .touchUpInside)
+  }
+
+  @objc private func convertButtonDidTap() {
+    guard let text = calculator.amountTextField.text,
+          let amoount = Double(text),
+          let code = countryCode,
+          let rate = rate else {
+      calculator.resultLabel.text = "올바른 숫자를 입력해 주세요"
+      return
+    }
+
+    let result = amoount * rate
+    calculator.resultLabel.text = "$" + format2f(amoount) + " → " + format2f(result) + " " + code
+  }
+
+  func format2f(_ text: Double) -> String {
+    return String(format: "%.2f", text)
   }
 }
+
